8b2787eeb2a92f0f940b5e02d9e17513338ac087
Bartosz Lenartowicz <218518@student.pwr.edu.pl>
2017-05-20 15:21:55 +0200

    poczatek

--- NEH/NEH/NEH.cpp
+++ NEH/NEH/NEH.cpp
@@ -2,10 +2,35 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-    return 0;
-}
+	std::vector<std::vector<int> > processingTimes;
 
+	// read input
+	int n, m;
+	std::cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		processingTimes.push_back(std::vector<int>());
+		for (int j = 0; j < m; j++)
+		{
+			int p;
+			std::cin >> p;
+			processingTimes[i].push_back(p);
+		}
+	}
+
+	// process
+	int cMax = -1;
+
+	// ...
+
+	// print result
+	std::cout << cMax << std::endl;
+
+	return 0;
+}
